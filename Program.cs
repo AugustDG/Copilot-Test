d8f158b3eac5d34ffc54a68dc210daaadae1be5f
AugustDG <augustomp55@gmail.com>
2021-07-17 12:39:08 -0400

    First upload error?

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Copilot_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Fibonacci.Generate();
+            Fibonacci.Generate();
+        }
+    }
+
+    // Generates Fibonacci numbers using iteration.
+    public static class Fibonacci
+    {
+        public static void Generate()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+            int c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+    }
+
+    //Generates decimals of Pi using iteration.
+}
